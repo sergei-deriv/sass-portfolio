8b3eeddf9341dd2a2c6150793362d000e3760686
Sergei Baranovski <sergei@deriv.com>
2022-12-19 14:33:45 +0300

    feat: show/hide navigation by burger click

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,23 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
 
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu)
-// hamburger.addEventListener('click', () => { alert('click'); })
 
 function toggleMenu() {
     if (!showMenu) {
         hamburger.classList.add('open');
-        // console.log('add');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
 
         showMenu = true;
     } else {
         hamburger.classList.remove('open');
-        // console.log('remove');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
 
         showMenu = false;
     }
